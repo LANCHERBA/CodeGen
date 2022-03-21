84e180952d6daf38093b577745173c3569a44136
Haoyang Ju <h6ju@uwaterloo.ca>
2022-03-21 02:21:41 -0400

    update ml-training script

--- ml-training.sh
+++ ml-training.sh
@@ -11,7 +11,36 @@ module load python/3.6 scipy-stack
 
 SOURCEDIR=/home/h6ju/projects/def-vganesh/h6ju/Transcoder/CodeGen
 
+MODEL=/home/h6ju/projects/def-vganesh/h6ju/Transcoder/CodeGen/TransCoder_model_1.pth
+
 source $SOURCEDIR/newCodeGen/bin/activate
 
-python -m codegen_sources.model.translate --src_lang java --tgt_lang cpp --model_path $SOURCEDIR/TransCoder_model_1.pth --beam_size 1 < $SOURCEDIR/data/Translation\ testing\ 2022-3-16/max+createDirectory/origin-max+createDirectory-java.java
+python codegen_sources/model/train.py \
+--exp_name transcoder_eval \
+--dump_path '/home/h6ju/projects/def-vganesh/h6ju/Transcoder/CodeGen/new_dump' \
+--data_path '/home/h6ju/projects/def-vganesh/h6ju/Transcoder/CodeGen/data/transcoder_evaluation_gfg' \
+--bt_steps 'python_sa-java_sa-python_sa,java_sa-python_sa-java_sa,python_sa-cpp_sa-python_sa,java_sa-cpp_sa-java_sa,cpp_sa-python_sa-cpp_sa,cpp_sa-java_sa-cpp_sa'    \
+--encoder_only False \
+--n_layers 0  \
+--n_layers_encoder 6  \
+--n_layers_decoder 6 \
+--emb_dim 1024  \
+--n_heads 8  \
+--lgs 'cpp_sa-java_sa-python_sa'  \
+--max_vocab 64000 \
+--gelu_activation false \
+--roberta_mode false  \
+--amp 2  \
+--fp16 true  \
+--tokens_per_batch 3000  \
+--max_batch_size 128 \
+--eval_bleu true \
+--eval_computation true \
+--has_sentence_ids "valid|para,test|para" \
+--generate_hypothesis true \
+--save_periodic 1 \
+--reload_model "$MODEL,$MODEL" \
+--reload_encoder_for_decoder false \
+--eval_only true \
+--n_sentences_eval 1500
 
